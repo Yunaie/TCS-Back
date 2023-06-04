4cd0ac226f83ca53c65eec827e72a498578697a5
Yunaie <119615317+Yunaie@users.noreply.github.com>
2023-06-04 20:11:20 +0200

    Update commentaire.js

--- routes/commentaire.js
+++ routes/commentaire.js
@@ -2,14 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const { createCommentaire,getCommentairesByArticle,getCommentairesByUser,getCommentaires,deleteCommentaire } = require('../controllers/commentaire');
-const {checkUser, requireAdmin} = require('../middleware/auth');
 
 
-router.post('/',checkUser, createCommentaire);
+router.post('/', createCommentaire);
 router.get('/', getCommentaires);
 router.post('/article',getCommentairesByArticle)
 router.post('/user',getCommentairesByUser)
-router.delete('/:id',checkUser,requireAdmin, deleteCommentaire)
+router.delete('/:id', deleteCommentaire)
 
 
 module.exports = router;
