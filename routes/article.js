59539e7081fcd641a3602c1d7e4023d368c56668
Yunaie <119615317+Yunaie@users.noreply.github.com>
2023-06-04 20:10:57 +0200

    Update article.js

--- routes/article.js
+++ routes/article.js
@@ -4,14 +4,13 @@ const article = require('../models/article');
 
 const router = express.Router();
 const { createArticle, getLatestArticles,getArticles,deleteArticle,getArticleById } = require('../controllers/article');
-const {checkUser, requireAdmin} = require('../middleware/auth');
 
 
 
-router.post('/', checkUser,requireAdmin,createArticle);
+router.post('/',createArticle);
 router.get('/all', getArticles);
 router.get('/',getLatestArticles);
 router.get("/:id",getArticleById);
-router.delete('/:id', checkUser,requireAdmin,deleteArticle);
+router.delete('/:id',deleteArticle);
 
 module.exports = router;
