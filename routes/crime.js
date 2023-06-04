8e93969f3f961339d242f7b35bf0e6853276dba7
Yunaie <119615317+Yunaie@users.noreply.github.com>
2023-06-04 20:11:37 +0200

    Update crime.js

--- routes/crime.js
+++ routes/crime.js
@@ -4,11 +4,10 @@ const crime = require('../models/crime');
 
 const router = express.Router();
 const { createCrime, getCrimes, deleteCrimes,getCrimeById } = require('../controllers/crime');
-const {checkUser, requireAdmin} = require('../middleware/auth');
 
-router.post('/',checkUser,requireAdmin, createCrime);
+router.post('/', createCrime);
 router.get('/', getCrimes);
-router.delete('/:id',checkUser,requireAdmin,deleteCrimes);
+router.delete('/:id',deleteCrimes);
 router.get('/:id',getCrimeById);
 
 module.exports = router;
