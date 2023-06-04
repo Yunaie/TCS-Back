86772f3c74df673f6fc92b4e743c44d7c58d391c
Yunaie <119615317+Yunaie@users.noreply.github.com>
2023-06-04 20:12:25 +0200

    Update victime.js

--- routes/victime.js
+++ routes/victime.js
@@ -4,12 +4,11 @@ const victime = require('../models/victime');
 
 const router = express.Router();
 const { createVictim, getVictim,deleteVictim,getVictimeById } = require('../controllers/victime');
-const {checkUser, requireAdmin} = require('../middleware/auth');
 
 
-router.post('/',checkUser, requireAdmin, createVictim);
+router.post('/', createVictim);
 router.get('/', getVictim);
-router.delete('/:id',checkUser, requireAdmin,deleteVictim);
+router.delete('/:id',deleteVictim);
 router.get('/:id',getVictimeById);
 
 module.exports = router;
